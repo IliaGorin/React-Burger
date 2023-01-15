3df4f01f1d5bc4a330ade0e49056d527439e5985
IliaGorin <101461772+IliaGorin@users.noreply.github.com>
2023-01-15 19:06:49 +0300

    added redux store and redux-dev-tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { Provider } from 'react-redux';
+import { rootReducer } from './services/reducers';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './components/app/app';
+
+const enhancer = composeWithDevTools(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
